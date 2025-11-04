0cfcc92fe7b9f42d7183e300f7685807d596e150
xserna656 <xserna2468@gmail.com>
2025-11-03 17:18:28 -0800

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,13 @@
+
+public class Static {
+
+    static int a = 0;
+    static int months = 12;
+
+    public static void main(String[] args) {
+        int b = 1;
+
+        System.out.println(a + b);
+    }
+
+}
