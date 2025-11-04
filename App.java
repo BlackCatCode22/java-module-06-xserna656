cc6deb06b7c3120bd5c1e37183a0941cb8744fab
xserna656 <xserna2468@gmail.com>
2025-11-03 17:20:13 -0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+
+   public class App {
+
+        public static void main(String[] args) {
+            Running r = new Running(4.5,"Nike", 10);
+            System.out.println(r.weight);
+        }
+
+
+
+   }
